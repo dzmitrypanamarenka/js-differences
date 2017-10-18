e43893dcfb95b4e2eaa13622ca44a089b5425dc0
dzmitry.panamarenka <dzmitry.panamarenka@gmail.com>
2017-10-18 18:10:22 +0300

    #diffs fixed import

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,10 +1,18 @@
 #!/usr/bin/env node
 
-const program = require('commander');
+import program from 'commander';
+import genDiff from '../';
 
 program
   .version('0.1.0')
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --foo', 'enable some foo')
-  .parse(process.argv);
+  .option('-f, --foo', 'enable some foo');
+
+program
+  .action((firstConfig, secondConfig) => {
+    const result = genDiff(firstConfig, secondConfig);
+    console.log(result);
+  });
+
+program.parse(process.argv);
