485416f3159376e7cabc27e714fcf28fd3492f20
dzmitry.panamarenka <dzmitry.panamarenka@gmail.com>
2017-10-18 14:50:29 +0300

    #diffs eslint updates

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,8 +3,8 @@
 const program = require('commander');
 
 program
-    .version('0.1.0')
-    .arguments('<firstConfig> <secondConfig>')
-    .description('Compares two configuration files and shows a difference.')
-    .option('-f, --foo', 'enable some foo')
-    .parse(process.argv);
+  .version('0.1.0')
+  .arguments('<firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --foo', 'enable some foo')
+  .parse(process.argv);
